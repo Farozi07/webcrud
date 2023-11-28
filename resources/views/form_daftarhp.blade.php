734cb2de99b32b013fb18baea458e3f0302dac19
Farozi07 <zideansaf320@gmail.com>
2023-11-28 11:32:51 +0700

    simpan git

--- resources/views/form_daftarhp.blade.php
+++ resources/views/form_daftarhp.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Pendaftaran HP</title>
+    <title>Daftar HP</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
